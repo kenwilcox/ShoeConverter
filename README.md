e25305e71add82288d01a2d83769afe7f26a84c9
kenwilcox <wilcoxk@gmail.com>
2014-09-13 09:41:42 -0600

    Added words to read me

---
+++ README.md
@@ -0,0 +1,1 @@
+Nothing fancy, Simple Swift app playing with numbers and text fields
--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var usShoeSizeTextField: UITextField!
+  @IBOutlet weak var euroShoeSizeTextField: UITextField!
+  
+  let usEuroDiff = 33
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,29 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-
+  @IBAction func usShoeSizeEditingChanged(sender: UITextField) {
+    let usShoeSizeString = usShoeSizeTextField.text
+    if (countElements(usShoeSizeString) > 0) {
+      let usShoeSize = usShoeSizeString.toInt()!
+      euroShoeSizeTextField.text = "\(usShoeSize + usEuroDiff)"
+    } else {
+      euroShoeSizeTextField.text = ""
+    }
+    
+  }
+  
+  @IBAction func euroShoeSizeEditingChanged(sender: UITextField) {
+    let euroShoeSizeString = euroShoeSizeTextField.text
+    if (countElements(euroShoeSizeString) > 0) {
+      let euroShoeSize = euroShoeSizeString.toInt()!
+      if (euroShoeSize > usEuroDiff) {
+        usShoeSizeTextField.text = "\(euroShoeSize - usEuroDiff)"
+      } else {
+        usShoeSizeTextField.text = ""
+      }
+    }
+    
+  }
+  
 }
 
