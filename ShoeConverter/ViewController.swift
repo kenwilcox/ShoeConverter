d9c198b0ed2e202c9e8b80df98841878c2c4a838
kenwilcox <wilcoxk@gmail.com>
2014-09-13 13:56:13 -0600

    Added Women's Shoes

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -12,8 +12,11 @@ class ViewController: UIViewController {
 
   @IBOutlet weak var usShoeSizeTextField: UITextField!
   @IBOutlet weak var euroShoeSizeTextField: UITextField!
+  @IBOutlet weak var sexSwitch: UISwitch!
+  @IBOutlet weak var conversionLabel: UILabel!
   
-  let usEuroDiff = 33
+  let usEuroMenDiff = 33.0
+  let usEuroWomenDiff = 30.5
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -25,28 +28,81 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
-  @IBAction func usShoeSizeEditingChanged(sender: UITextField) {
-    let usShoeSizeString = usShoeSizeTextField.text
-    if (countElements(usShoeSizeString) > 0) {
-      let usShoeSize = usShoeSizeString.toInt()!
-      euroShoeSizeTextField.text = "\(usShoeSize + usEuroDiff)"
+  func calculateMensShoeSize(euro: Bool) {
+    if euro {
+      let euroShoeSizeString = euroShoeSizeTextField.text
+      if (countElements(euroShoeSizeString) > 0) {
+        //let euroShoeSize = euroShoeSizeString.toInt()!
+        let euroShoeSize = NSString(string: euroShoeSizeString).doubleValue;
+        if (euroShoeSize > usEuroMenDiff) {
+          usShoeSizeTextField.text = "\(euroShoeSize - usEuroMenDiff)"
+        } else {
+          usShoeSizeTextField.text = ""
+        }
+      }
     } else {
-      euroShoeSizeTextField.text = ""
+      let usShoeSizeString = usShoeSizeTextField.text
+      if (countElements(usShoeSizeString) > 0) {
+        //let usShoeSize = usShoeSizeString.toInt()!
+        let usShoeSize = NSString(string: usShoeSizeString).doubleValue;
+        euroShoeSizeTextField.text = "\(usShoeSize + usEuroMenDiff)"
+      } else {
+        euroShoeSizeTextField.text = ""
+      }
     }
-    
+  }
+  
+  func calculateWomensShoeSize(euro: Bool) {
+    if euro {
+      let euroShoeSizeString = euroShoeSizeTextField.text
+      if (countElements(euroShoeSizeString) > 0) {
+        let euroShoeSize = NSString(string: euroShoeSizeString).doubleValue;
+        if (euroShoeSize > usEuroWomenDiff) {
+          usShoeSizeTextField.text = "\(euroShoeSize - usEuroWomenDiff)"
+        } else {
+          usShoeSizeTextField.text = ""
+        }
+      }
+    } else {
+      let usShoeSizeString = usShoeSizeTextField.text
+      if (countElements(usShoeSizeString) > 0) {
+        let usShoeSize = NSString(string: usShoeSizeString).doubleValue;
+        euroShoeSizeTextField.text = "\(usShoeSize + usEuroWomenDiff)"
+      } else {
+        euroShoeSizeTextField.text = ""
+      }
+    }
+  }
+  
+  @IBAction func sexSwitchValueChange(sender: UISwitch) {
+    if sexSwitch.on {
+      usShoeSizeTextField.placeholder = "US Womens Shoe Size"
+      euroShoeSizeTextField.placeholder = "Euro Womens Shoe Size"
+      conversionLabel.text = "Womens Shoes"
+      calculateWomensShoeSize(true)
+    } else {
+      usShoeSizeTextField.placeholder = "US Mens Shoe Size"
+      euroShoeSizeTextField.placeholder = "Euro Mens Shoe Size"
+      conversionLabel.text = "Mens Shoes"
+      calculateMensShoeSize(true)
+    }
+  }
+
+  
+  @IBAction func usShoeSizeEditingChanged(sender: UITextField) {
+    if sexSwitch.on {
+      calculateWomensShoeSize(false)
+    } else {
+      calculateMensShoeSize(false)
+    }
   }
   
   @IBAction func euroShoeSizeEditingChanged(sender: UITextField) {
-    let euroShoeSizeString = euroShoeSizeTextField.text
-    if (countElements(euroShoeSizeString) > 0) {
-      let euroShoeSize = euroShoeSizeString.toInt()!
-      if (euroShoeSize > usEuroDiff) {
-        usShoeSizeTextField.text = "\(euroShoeSize - usEuroDiff)"
-      } else {
-        usShoeSizeTextField.text = ""
-      }
+    if sexSwitch.on {
+      calculateWomensShoeSize(true)
+    } else {
+      calculateMensShoeSize(true)
     }
-    
   }
   
 }
